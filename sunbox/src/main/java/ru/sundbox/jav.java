8fbf5820f9c3d0405672c5721cb8b20a203d8521
KaterinaRomancheva <47745619+KaterinaRomancheva@users.noreply.github.com>
2019-02-21 13:47:30 +0300

    несколько функция

--- sunbox/src/main/java/ru/sundbox/jav.java
+++ sunbox/src/main/java/ru/sundbox/jav.java
@@ -1,8 +1,27 @@
 package ru.sundbox;
 
 public class jav {
-	
-	public static void main(String[] args) {
-System.out.print("hell77");
-	}
+
+   public static void main(String[] args) {
+      hello("1111");
+      hello("user");
+
+      double l = 5;
+      System.out.println("площадь квадрата со строной" + l + "равна" + area(l));
+      double a= 4;
+      double b = 6;
+      System.out.println("площадь прмоугольнка со строной" + a + "и"+ b + "равна" + area(a,b));
+
+   }
+
+   public static void hello(String somebody){
+
+      System.out.println("hello" + somebody);
+   }
+   public static double area(double a){
+      return   a*a;
+   }
+   public static double area (double a, double b) {
+      return a*b;
+   }
 }
